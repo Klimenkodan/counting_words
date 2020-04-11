a6df5bffe6e17d8c6038a2e8c29df11ff9a41fed
captainvlad <zadorozhnyy@ucu.edu.ua>
2020-04-11 23:03:25 +0300

    One thread function added

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/dan/Documents/Second_year/CSA/counting_words1
+# Install script for directory: /home/vlad/Desktop/2_year/ACS/lab_3/a/counting_words
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/dan/Documents/Second_year/CSA/counting_words1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/vlad/Desktop/2_year/ACS/lab_3/a/counting_words/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -12,6 +12,13 @@
 #include <tuple>
 #include <vector>
 
+
+#include <boost/locale.hpp>
+#include <boost/locale/boundary.hpp>
+#include <boost/locale/conversion.hpp>
+#include <fstream>
+#include <map>
+
 ///------------------------------------------------------------------------------------------------------------------///
 /// CONFIG FUNCTIONS
 
@@ -68,29 +75,67 @@ auto inline read_file_into_memory(const std::string &file_name) {
 	std::ifstream raw_file(file_name, std::ios::binary);
 	auto buffer = static_cast<std::ostringstream&>(
 			std::ostringstream{} << raw_file.rdbuf()).str();
-	std::cout << buffer << std::endl;
+
 	return buffer;
 
+}
+
+//--------------------------------------------------------------------------------------------------------------------//
+
+auto slice_text(std::string &text){
+
+    boost::locale::generator gen;
+    static auto loc = gen("en_US.UTF-8");
+
+    text = boost::locale::normalize(text, boost::locale::norm_nfd, loc);
+    text = boost::locale::fold_case(text, loc);
+
+    boost::locale::boundary::ssegment_index words( boost::locale::boundary::word, text.begin(), text.end(),loc );
+    words.rule(boost::locale::boundary::word_any);
+
+    return words;
+}
+
+auto count_words( boost::locale::boundary::ssegment_index &words_list ){
+    std::map<std::string, int> dictionary;
+
+    for ( auto word: words_list ){
+        dictionary[word] += 1;
+    }
+    return dictionary;
+}
+
+auto trigger_funtion(){
+//    one thread function
+
+    std::string tex = read_file_into_memory("/home/vlad/Desktop/2_year/ACS/lab_3/a/counting_words/file1.txt"); // your filepath here
+    auto txts = slice_text(tex);
+    auto dick = count_words(txts);
+
+    for (auto x: dick){
+        std::cout << " " << x.first << " " << x.second << "\n";
+    }
 }
 
 ///------------------------------------------------------------------------------------------------------------------///
 /// MAIN PROGRAM
 
 int main() {
+    trigger_funtion();
 
-	std::string file = "file.zip";
-	auto buf = read_file_into_memory(file);
-	struct archive *a = archive_read_new();
-	archive_read_support_compression_gzip(a);
-	archive_read_support_format_tar(a);
-	r = archive_read_open_memory(a, buff, sizeof(buff));
-	std::cout << buf << std::endl;
+//	std::string file = "file.zip";
+//	auto buf = read_file_into_memory(file);
+//	struct archive *a = archive_read_new();
+//	archive_read_support_compression_gzip(a);
+//	archive_read_support_format_tar(a);
+//	r = archive_read_open_memory(a, buff, sizeof(buff));
+//	std::cout << buf << std::endl;
 
 //	std::map<std::string, int> new_map;
 //	new_map["key"] = 12;
 //
 //	std::cout << new_map["key"] << std::endl;
-//	return 0;
+	return 0;
 }
 
 ///------------------------------------------------------------------------------------------------------------------///
